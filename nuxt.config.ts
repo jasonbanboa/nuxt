9e826d56eb00f04cd663786d082ea7772c9e5608
jasonbanboa <jasonbanboa123@gmail.com>
2023-11-12 21:29:26 +0900

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  alias: {
+    '@': resolve(__dirname, '/'),
+  },
+  devtools: { enabled: true },
+  css: ['~/assets/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
