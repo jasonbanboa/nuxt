2e1780f9a352e233de852c6efccc7a3e9f0331b5
jasonbanboa <jasonbanboa123@gmail.com>
2023-11-14 22:20:16 +0900

    state management using useState and pinia

---
+++ pages/pinia.vue
@@ -0,0 +1,16 @@
+<script setup>
+const store = useCounterStore();
+
+setTimeout(() => {
+  store.name = 'wow';
+}, 1500);
+
+</script>
+
+<template>
+  <button @click="store.increment">
+    click me!
+  </button>
+  {{ store.count }}
+  {{ store.name }}
+</template>
